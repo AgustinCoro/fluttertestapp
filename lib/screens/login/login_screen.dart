b41779a02247870b60adfa444412b3e77d64c589
AgustinCoro <102687327+AgustinCoro@users.noreply.github.com>
2023-12-03 16:17:52 -0300

    Agrego la pantalla de Login

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,17 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertestapp1/widgets/fondo_widget.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Inicio de Sesión'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: LoginForm(),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          FondoConContenedores(),
-        ],
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TextField(
+          controller: _emailController,
+          decoration: InputDecoration(labelText: 'Correo Electrónico'),
+          keyboardType: TextInputType.emailAddress,
+        ),
+        SizedBox(height: 16.0),
+        TextField(
+          controller: _passwordController,
+          decoration: InputDecoration(labelText: 'Contraseña'),
+          obscureText: true,
+        ),
+        SizedBox(height: 32.0),
+        ElevatedButton(
+          onPressed: () {
+            String email = _emailController.text;
+            String password = _passwordController.text;
+            print('Email: $email\nContraseña: $password');
+          },
+          child: Text('Iniciar Sesión'),
+        ),
+      ],
     );
   }
 }
